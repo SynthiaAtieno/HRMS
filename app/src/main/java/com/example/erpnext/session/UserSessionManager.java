a3f39127389d7c0831b666ef6d3917bf376a5ac3
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-02 11:10:48 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/session/UserSessionManager.java
+++ app/src/main/java/com/example/erpnext/session/UserSessionManager.java
@@ -9,6 +9,7 @@ public class UserSessionManager {
     private static final String KEY_USER_ID = "userId";
     private static final String KEY_FULL_NAME = "full_name";
     private static final String KEY_EMPLOYEE_NAMING_SERIES = "employee_naming";
+    private static final String USER_FIRST_NAME = "first_name";
 
     private final SharedPreferences sharedPreferences;
     private final SharedPreferences.Editor editor;
@@ -44,6 +45,15 @@ public class UserSessionManager {
 
     public String getKeyEmployeeNamingSeries(){
         return sharedPreferences.getString(KEY_EMPLOYEE_NAMING_SERIES, null);
+    }
+
+    public void setUserFirstName(String firstName){
+        editor.putString(USER_FIRST_NAME, firstName);
+        editor.apply();
+    }
+
+    public String getUserFirstName(){
+        return sharedPreferences.getString(USER_FIRST_NAME, null);
     }
     public boolean isLoggedIn() {
         return getUserId() != null && getFullName() != null;
