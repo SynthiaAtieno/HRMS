328d566de46c4294d06255b83bf769f84c86c6a9
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-02 11:09:40 +0300

    Made some changes

---
+++ app/src/main/java/com/example/erpnext/Greetings.java
@@ -0,0 +1,22 @@
+package com.example.erpnext;
+
+import java.util.Calendar;
+
+public class Greetings {
+
+    public static String getGreeting() {
+        Calendar calendar = Calendar.getInstance();
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        String greeting;
+        if (hourOfDay < 12) {
+            greeting = "Good morning, ";
+        } else if (hourOfDay < 17) {
+            greeting = "Good afternoon, ";
+        } else {
+            greeting = "Good evening, ";
+        }
+
+        return greeting;
+    }
+}
