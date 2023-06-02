310e7a8852e598e86332f68ecda7aa075d6372f5
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-02 11:08:44 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/fragments/profile/EducationFragment.java
+++ app/src/main/java/com/example/erpnext/fragments/profile/EducationFragment.java
@@ -1,66 +1,125 @@
 package com.example.erpnext.fragments.profile;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.erpnext.R;
+import com.example.erpnext.adapters.EmployeeEducationAdapter;
+import com.example.erpnext.models.EmployeesData;
+import com.example.erpnext.models.PermissionError;
+import com.example.erpnext.services.ApiClient;
+import com.example.erpnext.session.UserSessionManager;
+import com.google.gson.Gson;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EducationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
 public class EducationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    UserSessionManager sessionManager;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private EmployeeEducationAdapter adapter;
+    private List<EmployeesData.EmployeeDoc> employees;
 
     public EducationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EducationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EducationFragment newInstance(String param1, String param2) {
-        EducationFragment fragment = new EducationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        getEmployeeData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_education, container, false);
+        View view = inflater.inflate(R.layout.fragment_education, container, false);
+        return view;
+    }
+
+    public void getEmployeeData() {
+        //progressBar.setVisibility(View.VISIBLE);
+        sessionManager = new UserSessionManager(getContext());
+        ApiClient.getApiClient().getEmployeeData("Employee", sessionManager.getKeyEmployeeNamingSeries(), sessionManager.getUserId()).enqueue(new Callback<EmployeesData>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(Call<EmployeesData> call, Response<EmployeesData> response) {
+                if (response.isSuccessful()) {
+                    EmployeesData responseModel = response.body();
+                    if (responseModel != null && responseModel.getDocs() != null && !responseModel.getDocs().isEmpty()) {
+
+                        List<EmployeesData.EmployeeDoc> employees = responseModel.getDocs();
+
+                        for (EmployeesData.EmployeeDoc employee : employees) {
+                            List<EmployeesData.EmployeeEducation> educationList = employee.getEducation();
+
+                            for (EmployeesData.EmployeeEducation education : educationList) {
+                                String institutionName = education.getName();
+                                int yearOfPassing = education.getYearOfPassing();
+
+                                // Do something with the education data
+                                System.out.println("Institution Name: " + institutionName);
+                                System.out.println("Year of Passing: " + yearOfPassing);
+                            }
+                        }
+
+                    } else {
+                        Toast.makeText(getContext(), "Null data", Toast.LENGTH_SHORT).show();
+                        //progressBar.setVisibility(View.GONE);
+                    }
+
+                } else {
+                    if (response.errorBody() != null) {
+                        try {
+                            String errorResponseJson = response.errorBody().string();
+                            PermissionError errorResponse = new Gson().fromJson(errorResponseJson, PermissionError.class);
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                            builder.setTitle("Error Occurred");
+                            builder.setMessage(errorResponse.getExcType());
+
+                            // Set a positive button and its click listener
+                            builder.setPositiveButton("Dismiss", (dialog, which) -> dialog.dismiss());
+
+                            // Create and show the alert dialog
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                            //progressBar.setVisibility(View.GONE);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            // progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                    //Toast.makeText(MainActivity.this, "Response not successful", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<EmployeesData> call, Throwable t) {
+                Toast.makeText(getContext(), "Error occurred " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                System.out.println("Onfailure" + t.getMessage());
+                // progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
