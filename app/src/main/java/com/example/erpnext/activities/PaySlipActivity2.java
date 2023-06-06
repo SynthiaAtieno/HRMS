45a6d024ff0f1df06a559c6fed4dc66e1f120469
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-06 17:46:21 +0300

    Made some changes

---
+++ app/src/main/java/com/example/erpnext/activities/PaySlipActivity2.java
@@ -0,0 +1,25 @@
+package com.example.erpnext.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.erpnext.R;
+
+import java.util.Objects;
+
+public class PaySlipActivity2 extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_slip2);
+        toolbar = findViewById(R.id.payslip_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Payslip");
+
+    }
+}
