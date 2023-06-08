650c639aa9a13bbb7eb6f150e033e3cf26c876f7
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-08 13:40:30 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/activities/PaySlipActivity2.java
+++ app/src/main/java/com/example/erpnext/activities/PaySlipActivity2.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
+import com.example.erpnext.CustomAdapter;
 import com.example.erpnext.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class PaySlipActivity2 extends AppCompatActivity {
@@ -21,5 +25,27 @@ public class PaySlipActivity2 extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Payslip");
 
+        Spinner spinner = findViewById(R.id.spinner_item_text);  // Replace with your spinner's ID
+
+// Create a list of months
+        List<String> months = new ArrayList<>();
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+
+// Create an instance of your custom adapter
+        CustomAdapter adapter = new CustomAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, months);
+
+// Set the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
