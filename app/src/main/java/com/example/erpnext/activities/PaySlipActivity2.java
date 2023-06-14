5237bc5fa1ae0e1fd15f655fa60c38a2a1bfef44
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-14 17:24:34 +0300

    Made some changes"

--- app/src/main/java/com/example/erpnext/activities/PaySlipActivity2.java
+++ app/src/main/java/com/example/erpnext/activities/PaySlipActivity2.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import com.example.erpnext.CustomAdapter;
@@ -15,6 +17,7 @@ import java.util.Objects;
 
 public class PaySlipActivity2 extends AppCompatActivity {
     Toolbar toolbar;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,20 @@ public class PaySlipActivity2 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Payslip");
+
+        spinner = findViewById(R.id.spinner_item_text_month);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
