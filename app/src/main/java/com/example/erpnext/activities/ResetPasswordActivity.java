5eaf0b5340f66cc678c7a47bfd013343143e9614
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-14 13:06:00 +0300

    deleted some unnecessary import

--- app/src/main/java/com/example/erpnext/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/example/erpnext/activities/ResetPasswordActivity.java
@@ -10,7 +10,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 
 import com.example.erpnext.R;
 import com.google.android.material.textfield.TextInputLayout;
