826da77ea1cff60f54bfd91cf22c12477513c42d
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-14 12:46:20 +0300

    deleted some unnecessary import

--- app/src/main/java/com/example/erpnext/activities/Login.java
+++ app/src/main/java/com/example/erpnext/activities/Login.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
