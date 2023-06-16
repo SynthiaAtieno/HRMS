2496b6344a57168dfa35de173ed4fbc56205da6b
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-17 00:45:44 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/LeaveReportActivity.java
+++ app/src/main/java/com/example/erpnext/LeaveReportActivity.java
@@ -1,13 +1,17 @@
 package com.example.erpnext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.Objects;
 
 public class LeaveReportActivity extends AppCompatActivity {
+    AppCompatButton button;
 
     Toolbar toolbar;
     @Override
@@ -18,5 +22,14 @@ public class LeaveReportActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Leave Report");
+
+        button = findViewById(R.id.applyleavebtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LeaveReportActivity.this, ApplyLeaveActivity.class));
+            }
+        });
     }
 }
