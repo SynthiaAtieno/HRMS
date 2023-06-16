8506153cf0afa323a8b791590bc38a0ad77aa5ba
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-17 00:45:28 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/ApplyLeaveActivity.java
+++ app/src/main/java/com/example/erpnext/ApplyLeaveActivity.java
@@ -1,8 +1,11 @@
 package com.example.erpnext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class ApplyLeaveActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class ApplyLeaveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_leave);
+        Toolbar toolbar = findViewById(R.id.apply_leave_toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Apply Leave");
+
     }
 }
