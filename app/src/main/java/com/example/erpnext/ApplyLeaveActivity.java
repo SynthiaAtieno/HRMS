edea1a8d7bd78cfed4c3a9dbd6c7f68245608324
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-16 20:33:44 +0300

    Made some changes

---
+++ app/src/main/java/com/example/erpnext/ApplyLeaveActivity.java
@@ -0,0 +1,14 @@
+package com.example.erpnext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplyLeaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_leave);
+    }
+}
