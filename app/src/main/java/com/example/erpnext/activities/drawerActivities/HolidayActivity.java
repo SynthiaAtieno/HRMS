3e91d7b6a199bda568fecd000fef0fd3bed80322
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-18 13:47:39 +0300

    Made some changes

--- app/src/main/java/com/example/erpnext/activities/drawerActivities/HolidayActivity.java
+++ app/src/main/java/com/example/erpnext/activities/drawerActivities/HolidayActivity.java
@@ -1,36 +1,43 @@
 package com.example.erpnext.activities.drawerActivities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.erpnext.R;
+import com.example.erpnext.adapters.DateUtils;
 import com.example.erpnext.adapters.HolidayListAdapter;
 import com.example.erpnext.models.HolidayList;
 import com.example.erpnext.services.ApiClient;
 import com.example.erpnext.session.UserSessionManager;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class HolidayActivity extends AppCompatActivity {
     RecyclerView holidayrecycler;
-    HolidayListAdapter holidayListAdapter;
     List<HolidayList.Holiday> holidayLists;
     UserSessionManager sessionManager;
     ProgressBar progressBar;
-
+    SimpleDateFormat dateFormat;
+    private HolidayListAdapter holidayListAdapter;
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,32 +48,47 @@ public class HolidayActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Holiday List");
-        sessionManager = new UserSessionManager(this);
-        holidayrecycler = findViewById(R.id.holidayRecycler);
-        holidayLists = new ArrayList<>();
-        holidayrecycler.setLayoutManager(new LinearLayoutManager(this));
-        holidayListAdapter = new HolidayListAdapter(holidayLists, this);
 
+        holidayrecycler = findViewById(R.id.holidayRecycler);
         progressBar = findViewById(R.id.progressbarholiday);
         getHolidays();
     }
     public void getHolidays(){
+        sessionManager = new UserSessionManager(this);
         progressBar.setVisibility(View.VISIBLE);
         String doctype = "Holiday List";
         String holidayname = "2023-Holiday-List";
         ApiClient.getApiClient().getHolidayList(doctype, holidayname, sessionManager.getUserId()).enqueue(new Callback<HolidayList>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onResponse(Call<HolidayList> call, Response<HolidayList> response) {
                 if (response.isSuccessful()){
                     HolidayList holidayList = response.body();
                     if (holidayList != null && !holidayList.getDocs().isEmpty() && holidayList.getDocs() !=null){
+
+                        dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        holidayLists = new ArrayList<>();
+                        holidayListAdapter = new HolidayListAdapter(holidayLists, HolidayActivity.this);
                         HolidayList.Doc holiday = holidayList.getDocs().get(0);
+                        String currentDate = dateFormat.format(new Date());
+                        for (HolidayList.Holiday holiday1 : holiday.getHolidays()){
+                            if (holiday1.getHolidayDate().compareTo(currentDate)>0 || holiday1.getHolidayDate().compareTo(currentDate) == 0){
+                                String formattedDate = DateUtils.convertStringToDate(holiday1.getHolidayDate(), "yyyy-MM-dd", "dd MMM yyyy");
+                                holiday1.setHolidayDate(formattedDate);
+                                holidayLists.add(holiday1);
+
+                            }
+                        }
 
                         holidayrecycler.setAdapter(holidayListAdapter);
-                        holidayLists.addAll(holiday.getHolidays());
+                        holidayrecycler.setLayoutManager(new LinearLayoutManager(HolidayActivity.this));
+                        holidayrecycler.setHasFixedSize(true);
                         progressBar.setVisibility(View.GONE);
                         holidayListAdapter.notifyDataSetChanged();
+
+
                     }
+
                     else {
                         progressBar.setVisibility(View.GONE);
                         Toast.makeText(HolidayActivity.this, "No Data in Holiday List", Toast.LENGTH_SHORT).show();
