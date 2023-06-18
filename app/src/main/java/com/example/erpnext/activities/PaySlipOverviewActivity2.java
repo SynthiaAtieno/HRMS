a4ddd8acf1f31cf0267164811abe6926a5dda676
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-18 13:46:42 +0300

    Made some changes

---
+++ app/src/main/java/com/example/erpnext/activities/PaySlipOverviewActivity2.java
@@ -0,0 +1,17 @@
+package com.example.erpnext.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.erpnext.R;
+
+public class PaySlipOverviewActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_slip_overview2);
+    }
+}
+
