387b4db17ffdbe0ede69587a7e33e1a5419b30ab
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-18 13:48:00 +0300

    Made some changes

---
+++ app/src/main/java/com/example/erpnext/adapters/DateUtils.java
@@ -0,0 +1,26 @@
+package com.example.erpnext.adapters;
+
+import android.icu.text.SimpleDateFormat;
+import android.net.ParseException;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Date;
+
+public class DateUtils {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static String convertStringToDate(String dateString, String inputFormat, String outputFormat) {
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat(inputFormat);
+        SimpleDateFormat outputDateFormat = new SimpleDateFormat(outputFormat);
+
+        try {
+            Date date = inputDateFormat.parse(dateString);
+            return outputDateFormat.format(date);
+        } catch (ParseException | java.text.ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null; // Return null if parsing fails
+    }
+}
