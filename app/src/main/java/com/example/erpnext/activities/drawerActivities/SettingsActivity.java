9984197dec8960f5b63a46eb8ef2d3c6f010ff72
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-19 15:31:12 +0300

    Created theme for light mode

--- app/src/main/java/com/example/erpnext/activities/drawerActivities/SettingsActivity.java
+++ app/src/main/java/com/example/erpnext/activities/drawerActivities/SettingsActivity.java
@@ -3,7 +3,10 @@ package com.example.erpnext.activities.drawerActivities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.example.erpnext.R;
 
@@ -20,5 +23,19 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Settings");
+    }
+    private boolean isDarkThemePreferred() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String themePreference = sharedPreferences.getString("theme_preference", "system");
+
+        if (themePreference.equals("dark")) {
+            return true;
+        } else if (themePreference.equals("light")) {
+            return false;
+        } else {
+            // If the theme preference is set to "system", use the system default
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+        }
     }
 }
