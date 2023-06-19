532681aba1041b45bf67b26d26beef886878da83
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-19 15:29:51 +0300

    Created theme for light mode

--- app/src/main/java/com/example/erpnext/activities/drawerActivities/AppointmentsActivity.java
+++ app/src/main/java/com/example/erpnext/activities/drawerActivities/AppointmentsActivity.java
@@ -3,7 +3,10 @@ package com.example.erpnext.activities.drawerActivities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.example.erpnext.R;
 
@@ -14,12 +17,32 @@ public class AppointmentsActivity extends AppCompatActivity {
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (isDarkThemePreferred()) {
+            setTheme(R.style.AppTheme_Dark);
+        } else {
+            setTheme(R.style.AppTheme_Light);
+        }
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_appointments);
 
         toolbar = findViewById(R.id.appointment_toolbar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Appointments");
+    }
+    private boolean isDarkThemePreferred() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String themePreference = sharedPreferences.getString("theme_preference", "system");
+
+        if (themePreference.equals("dark")) {
+            return true;
+        } else if (themePreference.equals("light")) {
+            return false;
+        } else {
+            // If the theme preference is set to "system", use the system default
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+        }
     }
 }
