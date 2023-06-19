1552d2c34a9b27a231ff2e12eeff8e14c10e3f23
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-19 15:29:55 +0300

    Created theme for light mode

--- app/src/main/java/com/example/erpnext/activities/drawerActivities/BenefitsActivity.java
+++ app/src/main/java/com/example/erpnext/activities/drawerActivities/BenefitsActivity.java
@@ -3,7 +3,10 @@ package com.example.erpnext.activities.drawerActivities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.example.erpnext.R;
 
@@ -14,6 +17,11 @@ public class BenefitsActivity extends AppCompatActivity {
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (isDarkThemePreferred()) {
+            setTheme(R.style.AppTheme_Dark);
+        } else {
+            setTheme(R.style.AppTheme_Light);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benefits);
 
@@ -21,5 +29,19 @@ public class BenefitsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Benefits");
+    }
+    private boolean isDarkThemePreferred() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String themePreference = sharedPreferences.getString("theme_preference", "system");
+
+        if (themePreference.equals("dark")) {
+            return true;
+        } else if (themePreference.equals("light")) {
+            return false;
+        } else {
+            // If the theme preference is set to "system", use the system default
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
+        }
     }
 }
