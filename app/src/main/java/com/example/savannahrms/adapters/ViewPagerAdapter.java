e41a3467289d6f1c5fdd0a0cf9cd73edde762134
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-24 23:12:29 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/example/savannahrms/adapters/ViewPagerAdapter.java
@@ -1,4 +1,4 @@
-package com.example.erpnext.adapters;
+package com.example.savannahrms.adapters;
 
 
 import android.content.Context;
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
-import com.example.erpnext.R;
+import com.example.savannahrms.R;
 
 public class ViewPagerAdapter extends PagerAdapter {
     Context context;
