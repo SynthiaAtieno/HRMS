dc8c3973e0a75de4f13a938dae72612bec80a9b7
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-24 23:13:15 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/adapters/SlipDetailsAdapter.java
+++ app/src/main/java/com/example/savannahrms/adapters/SlipDetailsAdapter.java
@@ -1,4 +1,4 @@
-package com.example.erpnext.adapters;
+package com.example.savannahrms.adapters;
 
 import android.content.Context;
 import android.content.Intent;
@@ -7,16 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.erpnext.R;
-import com.example.erpnext.activities.PaySlipActivity2;
-import com.example.erpnext.models.SalarySlipReport;
+import com.example.savannahrms.R;
+import com.example.savannahrms.activities.PaySlipActivity2;
+import com.example.savannahrms.models.SalarySlipReport;
 
 import java.text.NumberFormat;
 import java.util.Currency;
