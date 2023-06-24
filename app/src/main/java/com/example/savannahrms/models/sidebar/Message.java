4ea052b28e84083023373596a5884bac1f91f889
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-24 23:16:47 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/models/sidebar/Message.java
+++ app/src/main/java/com/example/savannahrms/models/sidebar/Message.java
@@ -1,4 +1,4 @@
-package com.example.erpnext.models.sidebar;
+package com.example.savannahrms.models.sidebar;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
