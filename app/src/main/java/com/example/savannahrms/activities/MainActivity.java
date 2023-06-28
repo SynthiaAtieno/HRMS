a3e6215d89dc25a15b45d7206bbfd2dc00ecccff
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-28 22:23:34 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/activities/MainActivity.java
+++ app/src/main/java/com/example/savannahrms/activities/MainActivity.java
@@ -1,18 +1,5 @@
 package com.example.savannahrms.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,6 +13,19 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.savannahrms.R;
 import com.example.savannahrms.activities.drawerActivities.AppointmentsActivity;
@@ -136,6 +136,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    return true;
+
+                case R.id.attendance:
+                    replaceFragment(new AttendanceFragment());
+                    return true;
+
+                case R.id.leave:
+                    replaceFragment(new LeaveFragment());
+                    return true;
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    return true;
+
+                case R.id.mark_attendance:
+                    replaceFragment(new MarkAttendanceFragment());
+                    return true;
+            }
+            return true;
+        });
+
+    }
 
     @Override
     public void onBackPressed() {
