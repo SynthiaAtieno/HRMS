351ece34a67ba5b20cf05957a71c12f3bcf2c99b
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-29 22:21:12 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/activities/MainActivity.java
+++ app/src/main/java/com/example/savannahrms/activities/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     Toolbar toolbar;
     UserSessionManager sessionManager;
     RelativeLayout errorlayout;
-    TextView roletxt, permissiontxt;
+    TextView roletxt;
     FrameLayout frameLayout;
     View customView, logoutview;
 
