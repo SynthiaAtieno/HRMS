045c170109d1244ca55726834d09c1841f01b0a1
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-07-05 23:59:28 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/example/savannahrms/activities/ResetPasswordActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 
 
