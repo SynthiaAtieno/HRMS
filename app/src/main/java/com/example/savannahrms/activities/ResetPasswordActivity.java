b07a354dd815c6d15e242418849f9cd406468e6b
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-07-06 00:01:19 +0300

    Made some changes

--- app/src/main/java/com/example/savannahrms/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/example/savannahrms/activities/ResetPasswordActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 //import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
